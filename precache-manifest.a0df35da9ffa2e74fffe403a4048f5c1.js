d185a428c23a74c35cc65cc6933004a6e054284c
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-10-21 20:53:24 -0500

    Updates

---
+++ precache-manifest.a0df35da9ffa2e74fffe403a4048f5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d7e7fc3ea22d43794abfc59c4aa616e",
+    "url": "/Twitter-clone-app/index.html"
+  },
+  {
+    "revision": "fd49a1087eaf1112a636",
+    "url": "/Twitter-clone-app/static/css/main.61128a95.chunk.css"
+  },
+  {
+    "revision": "5bc8757bda4a4140f78d",
+    "url": "/Twitter-clone-app/static/js/2.fd5b9c9b.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/Twitter-clone-app/static/js/2.fd5b9c9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd49a1087eaf1112a636",
+    "url": "/Twitter-clone-app/static/js/main.ed02e098.chunk.js"
+  },
+  {
+    "revision": "e5fc641f1fbe9db31666",
+    "url": "/Twitter-clone-app/static/js/runtime-main.3d3c3aff.js"
+  }
+]);
